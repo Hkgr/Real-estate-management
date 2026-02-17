5948e72b4258e4124627d759774f7c4e7199a67d
Rashad Al-Shiekh <rashad000089@gmail.com>
2026-02-17 15:55:13 +0300

    not fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         // يطبق على كل مسارات الويب (ومنها Filament panel)
         $middleware->appendToGroup('web', \App\Http\Middleware\NormalizeDigits::class);
 
