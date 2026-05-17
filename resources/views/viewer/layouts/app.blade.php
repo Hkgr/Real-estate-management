a723b0780d72da4ac0dac677290581ff399e63cc
Rashad Al-Shiekh <rashad000089@gmail.com>
2026-05-18 01:05:25 +0300

    fix(viewer): load all necessary assets for viewer layout

--- resources/views/viewer/layouts/app.blade.php
+++ resources/views/viewer/layouts/app.blade.php
@@ -6,15 +6,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Viewer New')</title>
 
-    @vite(['resources/css/viewer/app.css', 'resources/js/viewer/app.js'])
-    @if (request()->routeIs('viewer-new.hub'))
-        @vite(['resources/css/viewer/hub.css', 'resources/js/viewer/hub.js'])
-    @endif
+    @vite([
+        'resources/css/viewer/app.css',
+        'resources/js/viewer/app.js',
+        'resources/css/viewer/hub.css',
+        'resources/js/viewer/hub.js',
+        'resources/css/viewer/reports.css',
+        'resources/js/viewer/reports.js',
+    ])
 
-    @if (request()->routeIs('viewer-new.reports'))
-        @vite(['resources/css/viewer/reports.css', 'resources/js/viewer/reports.js'])
-    @endif
-    @stack('viewer-vite')
 </head>
 <body dir="rtl">
     @yield('content')
