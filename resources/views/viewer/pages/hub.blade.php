3697935cfb13449bcd593c5bb3a819053af2de64
Rashad Al-Shiekh <rashad000089@gmail.com>
2026-05-18 00:36:22 +0300

    fix(viewer): load standalone page assets from layout by route

--- resources/views/viewer/pages/hub.blade.php
+++ resources/views/viewer/pages/hub.blade.php
@@ -2,10 +2,6 @@
 
 @section('title', 'Viewer New Hub')
 
-@push('viewer-vite')
-    @vite(['resources/css/viewer/hub.css', 'resources/js/viewer/hub.js'])
-@endpush
-
 @section('content')
     @include('viewer.components.hub.background')
     @include('viewer.components.hub.topbar')
